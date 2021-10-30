07ac6325382e652e28890f094e873037a9d971eb
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-10-31 01:12:26 +0900

    create account page.

--- page/Account.tsx
+++ page/Account.tsx
@@ -1,11 +1,35 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 
 const Account = () => {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Settings!</Text>
-    </View>
+    <>
+      <View
+        style={{
+          backgroundColor: "#1DA1F2",
+          height: 120,
+          paddingHorizontal: 20,
+        }}
+      ></View>
+      <View
+        style={{
+          marginTop: -50,
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+      >
+        <Image
+          style={{
+            height: 100,
+            width: 100,
+            borderColor: "#1DA1F2",
+            borderWidth: 2,
+            borderRadius: 50,
+          }}
+          source={require("../assets/icon.png")}
+        />
+      </View>
+    </>
   );
 };
 
